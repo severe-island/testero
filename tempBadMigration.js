0e2a25d05f06e8f9a84ec2e04c74a23c3b600d77
2DKot <sparhoks@mail.ru>
2015-03-27 11:05:46 +0300

    Поправил миграцию. При удалении несуществующей коллекции, выкидывается ошибка 'ns not found'. Если эта ошибка возникает, или вообще нет ошибок, то создаём пользователей.

--- tempBadMigration.js
+++ tempBadMigration.js
@@ -6,18 +6,21 @@
 
 var db = require('./lib/dbtestero');
 var userCollection = db.connection.collections['users'];
+
 userCollection.drop(function(err){
-	if (!err)
-	{
-		console.log("userCollection is empty!");
-		db.addNewUser('Samuray@mail.cc', 'Harakiri', function() {
-		console.log("First template has been added")});
-
-		db.addNewUser('doctorHarrison@yahoo.com', 'killers-lobsters', function() {
-		console.log("Second template has been added")});
-
-		db.addNewUser('snowlyBlack@yahoo.com', 'sssAAAlll', function() {
-		console.log("Third template has been added")});
+  //если коллекции нет, то она выкидвает ошибку "ns not found".
+  if (!err || err.message=="ns not found"){ 
+    console.log("userCollection is empty!");
+    db.addNewUser('Samuray@mail.cc', 'Harakiri', function() {
+      console.log("First template has been added")});
+    
+    db.addNewUser('doctorHarrison@yahoo.com', 'killers-lobsters', function() {
+      console.log("Second template has been added")});
+    
+    db.addNewUser('snowlyBlack@yahoo.com', 'sssAAAlll', function() {
+      console.log("Third template has been added")});
 	}
+	else{
+    console.log(err); 
+  }
 });
-
