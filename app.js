6f5d3713f1552a68605bb0ec61c2197296a6004d
2DKot <sparhoks@mail.ru>
2015-03-29 13:19:10 +0300

    Удалил ненужные файлы от шаблона проекта.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var bodyParser = require('body-parser');
 var db = require('./lib/dbtestero'); 
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -30,7 +29,6 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
