6a17530731bc4c6e6716e279703bc78fbe89431e
Evgeniy A. Simonenko <easimonenko@mail.ru>
2015-03-31 22:38:46 +0300

    Улучшил сигнализацию в форме login. Подправил также код формы signup.

--- modules/users/js/login.js
+++ modules/users/js/login.js
@@ -18,3 +18,47 @@ $("#login-form").submit(function() {
   });
   return false;
 }); 
+
+$("#login-form #email").blur(function() {
+  if (!$(this).val())
+  {
+    $("#login-form #form-group-email").addClass("has-error");
+    $(this).next("span").addClass("glyphicon-remove");
+  }
+  else
+  {
+    $("#login-form #form-group-email").removeClass("has-error");
+    $("#login-form #form-group-email").addClass("has-success");
+    $(this).next("span").removeClass("glyphicon-remove");
+    $(this).next("span").addClass("glyphicon-ok");
+  }
+});
+
+$("#login-form #password").blur(function() {
+  if (!$(this).val())
+  {
+    $("#login-form #form-group-password").addClass("has-error");
+    $(this).next("span").addClass("glyphicon-remove");
+  }
+  else
+  {
+    $("#login-form #form-group-password").removeClass("has-error");
+    $("#login-form #form-group-password").addClass("has-success");
+    $(this).next("span").removeClass("glyphicon-remove");
+    $(this).next("span").addClass("glyphicon-ok");
+  }
+});
+
+$("#login-form #email").focus(function() {
+  $("#login-form #form-group-email").removeClass("has-error");
+  $("#login-form #form-group-email").removeClass("has-success");
+  $(this).next("span").removeClass("glyphicon-remove");
+  $(this).next("span").removeClass("glyphicon-ok");
+});
+
+$("#signup-form #password").focus(function() {
+  $("#login-form #form-group-password").removeClass("has-error");
+  $("#login-form #form-group-password").removeClass("has-success");
+  $(this).next("span").removeClass("glyphicon-remove");
+  $(this).next("span").removeClass("glyphicon-ok");
+});
