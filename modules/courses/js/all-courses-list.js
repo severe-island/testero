f35573bd2dc2372b79dc5e8fe1d8bd5ec41f7dd7
Evgeniy A. Simonenko <easimonenko@mail.ru>
2015-04-11 01:04:28 +0300

    Начал работу над выводом списка курсов. Обнаружен баг. См. issue.

--- modules/courses/js/all-courses-list.js
+++ modules/courses/js/all-courses-list.js
@@ -5,3 +5,21 @@ $("#all-courses-list #courses-menu").click(function() {
     .show("slow");
   return false;
 });
+
+$.ajax({
+    type: "POST",
+    url: "/courses/findAllCourses",
+    success: function(data)
+    {
+      if (!data.status) {
+        bootstrapAlert(data.msg, "warning", 2000000, function() {
+          return;
+        });
+      }
+      else {
+        bootstrapAlert(data.msg + " " + data.courses.length, "info", 2000000, function() {
+          return;
+        });
+      }
+    }
+  });
