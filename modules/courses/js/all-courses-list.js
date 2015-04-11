465e9e277436880e7795b0324e16e55913cdd618
Evgeniy A. Simonenko <easimonenko@mail.ru>
2015-04-11 19:44:42 +0300

    Доработано отображение списка курсов.

--- modules/courses/js/all-courses-list.js
+++ modules/courses/js/all-courses-list.js
@@ -1,37 +1,38 @@
-$("#all-courses-list #courses-menu").click(function() {
+$("#all-courses-list #courses-menu").click(function () {
   $("#content")
-    .hide("slow", function() {
+    .hide("slow", function () {
       $(this)
-      .html(app.modules.courses.html["menu"])
-      .slideDown("slow");
+        .html(app.modules.courses.html["menu"])
+        .slideDown("slow");
     });
-    
+
   return false;
 });
 
 $.ajax({
-    type: "POST",
-    url: "/courses/findAllCourses",
-    success: function(data)
-    {
-      //$("#content").hide("slow", function() {
-          //$(this).html(app.modules.courses.html["all-courses-list"]);
-          if (!data.status) {
-            $("#content #alert")
-              .addClass("alert-error")
-              .html(msg);
-          }
-          else if (data.courses.length === 0) {
-            $("#content #alert")
-              .addClass("alert-warning")
-              .html("Пока не зарегистрировано ни одного курса.");
-          }
-          else {
-            $("#content #alert")
-              .addClass("alert-info")
-              .html("Здесь будет список курсов...");
-          }
-          $("#content").slideDown("slow");
-      //});
+  type: "POST",
+  url: "/courses/findAllCourses",
+  success: function (data)
+  {
+    if (!data.status) {
+      $("#content #alert")
+        .addClass("alert-error")
+        .html(msg);
     }
+    else if (data.courses.length === 0) {
+      $("#content #alert")
+        .addClass("alert-warning")
+        .html("Пока не зарегистрировано ни одного курса.");
+    }
+    else {
+      $("#content #alert")
+        .addClass("alert-info")
+        .html("Здесь будет список курсов...");
+      for (var i = 0; i < data.courses.length; i++) {
+        $("#content #list table td").last().append("<td>" + data.courses[i].title + "</td>");
+      }
+      
+    }
+    $("#content").slideDown("slow");
+  }
 });
