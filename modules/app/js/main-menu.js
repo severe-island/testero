2ad11f78faa17c880c1f0bb8590843548f40d1b2
Evgeniy A. Simonenko <easimonenko@mail.ru>
2015-04-11 22:38:56 +0300

    Добавил вывод всех пользователей системы.

--- modules/app/js/main-menu.js
+++ modules/app/js/main-menu.js
@@ -1,6 +1,17 @@
 $("#content #main-menu #courses-item").click(function () {
-  $("#content").hide("slow")
-    .html(app.modules.courses.html["menu"])
-    .slideDown("slow");
+  $("#content").hide("slow", function() {
+    $(this)
+      .html(app.modules.courses.html["menu"])
+      .slideDown("slow");
+  });
+  return false;
+});
+
+$("#content #main-menu #users-item").click(function () {
+  $("#content").hide("slow", function() {
+    $(this)
+      .html(app.modules.users.html["menu"])
+      .slideDown("slow");
+  });
   return false;
 });
---
+++ modules/users/js/menu.js
@@ -0,0 +1,24 @@
+$("#users-menu #users-list-item").click(function() {
+  $("#content").hide("slow", function() {
+    $(this)
+      .html(app.modules.users.html["users-list"]);
+  });
+  return false;
+});
+
+$("#users-menu #add-user-item").click(function() {
+  $("#content").hide("slow", function() {
+    $(this)
+      .html(app.modules.users.html["add-user"]);
+  });
+  return false;
+});
+
+$("#users-menu #my-profile-item").click(function() {
+  $("#coontent").hide("slow", function() {
+    $(this)
+      .html(app.modules.users.html["my-profile"])
+      .show("slow");
+  });
+  return false;
+});
