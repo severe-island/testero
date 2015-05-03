b758181d199aee599d7764bf1ca262f65c3ed946
2DKot <sparhoks@mail.ru>
2015-05-03 12:20:32 +0300

    Вынес смену пароля в /users/updatePassword

--- modules/users/route/profile.js
+++ modules/users/route/profile.js
@@ -34,6 +34,99 @@ router.post('/updateProfile', function(req, res, next) {
   if(req.body.showEmail!==undefined) {
     updater.showEmail = req.body.showEmail;
   }
+  
+  
+  db.findUserByEmail(req.session.email, function(err, user) {
+    if(err) {
+      res.json({
+        status: false,
+        level: "danger",
+        msg: "Ошибка БД: " + err.message
+      });
+      return;
+    }
+    if(!user) {
+      res.json({
+        status: false,
+        level: "danger",
+        msg: "Вы не найдены в базе! Скорее всего, ошибка с сессией."
+      })
+      return;
+    }
+    if(!user.isAdministrator && req.session.email != req.body.email) {
+      res.json({
+        status: false,
+        level: "danger",
+        msg: "Только администратор может обновить чужой профиль!"
+      })
+      return;
+    }
+    db.findUserByEmail(req.body.email, function(err, targetUser) {
+      if(err) {
+        res.json({
+          status: false,
+          level: "danger",
+          msg: "Ошибка БД: " + err.message
+        });
+        return;
+      }
+      if(!user) {
+        res.json({
+          status: false,
+          level: "danger",
+          msg: "Пользователь " + req.body.email + " не найден!"
+        })
+        return;
+      }
+      if(updater.password && req.body.oldPassword != targetUser.password)
+      {
+        res.json({
+          status: false,
+          level: "danger",
+          msg: "Неверный пароль!"
+        });
+        return;
+      }
+      
+      db.updateUser(req.body.email, updater, req.session.email, function(err, numUpdated) {
+        if(err) {
+          res.json({
+            status: false,
+            level: "danger",
+            msg: "Ошибка БД: " + err.message
+          });
+          return;
+        }
+        res.json({
+          status: true,
+          level: "success",
+          msg: "Профиль успешно обновлён!: "
+        });
+      })
+    })
+  })
+})
+
+router.post('/updatePassword', function(req, res, next) {
+  if(!req.session.login) {
+    res.json({
+      status: false,
+      level: "danger",
+      msg: "Функция доступна только зарегистрированным пользователям!"
+    });
+    return;
+  }
+  if(!req.body.email) {
+    res.json({
+      status: false,
+      level: "danger",
+      msg: "Не передан email!"
+    });
+    return;
+  }
+  
+  var updater = { };
+  
   if(req.body.password) {
     if(!req.body.passwordDuplicate) {
       res.json({
@@ -132,6 +225,6 @@ router.post('/updateProfile', function(req, res, next) {
       })
     })
   })
-})
+});
 
 module.exports = router;
