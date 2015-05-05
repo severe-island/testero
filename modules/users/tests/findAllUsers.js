3b1ffd23aa41e8890e6f079ce22d84e93a5a67d3
Evgeniy A. Simonenko <easimonenko@mail.ru>
2015-05-05 23:03:47 +0300

    Странная работа тестирования.

--- modules/users/tests/findAllUsers.js
+++ modules/users/tests/findAllUsers.js
@@ -1,10 +1,10 @@
 var request = require('supertest');
 var app = require('../../../app');
-var userConst = require('../js/const');
+var usersConst = require('../js/const');
 
-describe('Модуль users:', function () {
-  describe('Список всех пользователей:', function() {
-    it('Список пуст:', function (done) {
+describe('Модуль users', function () {
+  describe('Список всех пользователей', function() {
+    /*it('Список пуст', function (done) {
       request(app)
         .post('/users/findAllUsers')
         .set('X-Requested-With', 'XMLHttpRequest')
@@ -17,11 +17,48 @@ describe('Модуль users:', function () {
           res.body.status.should.equal(true);
           res.body.users.should.be.an.instanceOf(Array).and.have.lengthOf(0);
           done();
-          console.log(userConst.Messages.findAllUsers[userConst.ErrorCodes.findAllUsers.SUCCESSFUL]);
+          //console.log(usersConst.Messages.findAllUsers[usersConst.ErrorCodes.findAllUsers.SUCCESSFUL]);
         });
+    });*/
+    
+    it('Добавлен первый пользователь', function() {
+      var user = {
+        email: "user1@testero",
+        password: "user1",
+        passwordDuplicate: "user1"
+      };
+      request(app)
+        .post('/users/signup')
+        //.send(user)
+        .set('X-Requested-With', 'XMLHttpRequest')
+        .expect('Content-Type', /application\/json/)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          res.body.should.have.property('blablabla');
+          res.body.data.status.should.equal(false);
+          done();
+          
+          /*request(app)
+            .post('/users/findAllUsers')
+            .set('X-Requested-With', 'XMLHttpRequest')
+            .expect('Content-Type', /application\/json/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) {
+                throw err;
+              }
+              res.body.status.should.equal(true);
+              res.body.users.should.be.an.instanceOf(Array).and.have.lengthOf(1);
+              done();
+              //console.log(res.body.users.length);
+          });*/
+      });
     });
     
-    it('Добавлен первый пользователь:', function() {
+    /*it('Добавлен второй пользователь', function() {
       var user = {
         email: "user1@testero",
         password: "user1",
@@ -38,28 +75,10 @@ describe('Модуль users:', function () {
             throw err;
           }
           res.body.status.should.equal(true);
-          
-          request(app)
-            .post('/users/findAllUsers')
-            .set('X-Requested-With', 'XMLHttpRequest')
-            .expect('Content-Type', /application\/json/)
-            .expect(200)
-            .end(function(err, res) {
-              if (err) {
-                throw err;
-              }
-              res.body.status.should.equal(true);
-              res.body.users.should.be.an.instanceOf(Array).and.have.lengthOf(0);
-              done();
-          });
-          
           done();
       });
-    });
-    
-    it('Добавлен второй пользователь:', function() {
       request(app)
-        .post('/users/signup')
+        .post('/users/findAllUsers')
         .set('X-Requested-With', 'XMLHttpRequest')
         .expect('Content-Type', /application\/json/)
         .expect(200)
@@ -67,24 +86,12 @@ describe('Модуль users:', function () {
           if (err) {
             throw err;
           }
+          
           res.body.status.should.equal(true);
-          
-          request(app)
-            .post('/users/findAllUsers')
-            .set('X-Requested-With', 'XMLHttpRequest')
-            .expect('Content-Type', /application\/json/)
-            .expect(200)
-            .end(function(err, res) {
-              if (err) {
-                throw err;
-              }
-              res.body.status.should.equal(true);
-              res.body.users.should.be.an.instanceOf(Array).and.have.lengthOf(0);
-              done();
-          });
-          
+          res.body.users.should.be.an.instanceOf(Array).and.have.lengthOf(2);
           done();
+          
       });
-    });
+    });*/
   });
 });
