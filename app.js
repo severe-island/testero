5401cea2289b8243cedc00324eb4594e6906c29c
Evgeniy A. Simonenko <easimonenko@mail.ru>
2017-07-08 15:32:25 +0300

    Added favicon.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+const favicon = require('serve-favicon')
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -16,6 +16,7 @@ var app = express();
 if(config.mode !== "testing") {
   app.use(logger('dev'));
 }
+app.use(favicon(__dirname + '/public/favicon.ico'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
