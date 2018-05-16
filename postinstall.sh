2767932459951b5f9cbcb0a89c6d62ef8b1b6a21
Evgeny Simonenko <easimonenko@mail.ru>
2018-05-16 13:41:38 +0300

    Reinstall Bootstrap 3.

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 
-ln -s $PWD/node_modules/jquery/dist/ ./public/lib/jquery
+ln -s $PWD/node_modules/bootstrap/dist ./public/lib/bootstrap
+ln -s $PWD/node_modules/jquery/dist ./public/lib/jquery
 ln -s $PWD/node_modules/jquery.loadtemplate/dist ./public/lib/jquery.loadtemplate
