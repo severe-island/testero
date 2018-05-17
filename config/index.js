798e626e816fcef0d74161008d6f2c778a6dabc7
Evgeny Simonenko <easimonenko@mail.ru>
2018-05-17 22:49:32 +0300

    No longer needed because of usage of the package 'config'.

--- config/index.js
+++
@@ -1,5 +0,0 @@
-var env = process.env.MODE || 'development' //извлекаем имя конфига из окружения,
-                                            //а по-умолчанию будет 'development'
-var cfg = require('./'+env+'.json')
-
-module.exports = cfg
