22b87bc4c9905adb69351a287cfad57e881d8b1a
Evgeny Simonenko <easimonenko@mail.ru>
2018-06-21 00:03:44 +0300

    Move adding course and user from menu to subpage.

--- public/courses/js/courses.js
+++ public/courses/js/courses.js
@@ -1,40 +1,51 @@
+"use strict"
+
+/* global $ showAlert */
+
 $.ajax({
   type: "GET",
   url: "/courses/courses",
   success: function (data)
   {
-    if (!data.status) {
-      showAlert(data.msg, data.level, 0);
-    }
-    else if (data.courses.length === 0) {
-      $('#page-content').loadTemplate(
-        '/courses/html/courses-empty.html', {}, {append: true}
-      );
-    }
-    else {
-      $('#page-content').loadTemplate(
-        '/courses/html/courses.html',
-        {},
-        {
-          append: true,
-          success: function() {
-            for (var i = 0; i < data.courses.length; i++) {
-              let course = data.courses[i]
-              $("#courses-list table tbody")
-                .loadTemplate('/courses/html/courses-item.html',
-                  {
-                    number: i + 1,
-                    title: course.title,
-                    authors: "",
-                    id: course.id,
-                    href: "/#!courses/course?id=" + course.id
-                  },
-                  {
-                    append: true
-                  });
-            }
-        }}
-      );
-    }
+    $('#page-content').loadTemplate(
+      '/courses/html/courses-menu.html',
+      {},
+      {
+        append: true,
+        success: () => {
+          if (!data.status) {
+            showAlert(data.msg, data.level, 0);
+          }
+          else if (data.courses.length === 0) {
+            $('#page-content').loadTemplate(
+              '/courses/html/courses-empty.html', {}, {append: true}
+            );
+          }
+          else {
+            $('#page-content').loadTemplate(
+              '/courses/html/courses.html',
+              {},
+              {
+                append: true,
+                success: function() {
+                  for (var i = 0; i < data.courses.length; i++) {
+                    let course = data.courses[i]
+                    $("#courses-list table tbody")
+                      .loadTemplate('/courses/html/courses-item.html',
+                        {
+                          number: i + 1,
+                          title: course.title,
+                          authors: "",
+                          id: course.id,
+                          href: "/#!courses/course?id=" + course.id
+                        },
+                        {
+                          append: true
+                        });
+                  }
+              }});
+          }
+        }
+      })
   }
 });
--- public/courses/js/teachers.js
+++ public/courses/js/teachers.js
@@ -1,49 +1,59 @@
 "use strict"
 
+/* global $ showAlert */
+
 $.ajax({
     type: "GET",
     url: "/courses/roles/?role=teacher",
     success: function (data) {
-      if (!data.status) {
-        showAlert(data.msg, data.level, 0);
-      }
-      else if (data.users.length === 0) {
-        $('#page-content').loadTemplate(
-          '/courses/html/teachers-empty-list.html', {}, {append: true}
-        );
-      }
-      else {
-        getUsers(data.users, (users) => {
-          $('#page-content').loadTemplate(
-            '/courses/html/teachers.html',
-            {},
-            {
-              append: true,
-              success: function() {
-                for (var i = 0; i < users.length; i++) {
-                  let user = users[i]
-                  $("#teachers-list table tbody")
-                    .loadTemplate('/courses/html/teachers-item.html',
-                      {
-                        number: i + 1,
-                        id: user.id,
-                        email: user.email,
-                        name: user.name,
-                        familyName: user.familyName,
-                        patronymic: user.patronymic,
-                        isAdministrator: user.isAdministrator
-                          ?'<span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>'
-                          : "",
-                        href: "/#!users/profile?id=" + user.id
-                      },
-                      {
-                        append: true
-                      });
-                }
-            }}
-          );
+      $('#page-content').loadTemplate(
+        '/courses/html/teachers-menu.html',
+        {},
+        {
+          append: true,
+          success: () => {
+            if (!data.status) {
+              showAlert(data.msg, data.level, 0);
+            }
+            else if (data.users.length === 0) {
+              $('#page-content').loadTemplate(
+                '/courses/html/teachers-empty-list.html', {}, {append: true}
+              );
+            }
+            else {
+              getUsers(data.users, (users) => {
+                $('#page-content').loadTemplate(
+                  '/courses/html/teachers.html',
+                  {},
+                  {
+                    append: true,
+                    success: function() {
+                      for (var i = 0; i < users.length; i++) {
+                        let user = users[i]
+                        $("#teachers-list table tbody")
+                          .loadTemplate('/courses/html/teachers-item.html',
+                            {
+                              number: i + 1,
+                              id: user.id,
+                              email: user.email,
+                              name: user.name,
+                              familyName: user.familyName,
+                              patronymic: user.patronymic,
+                              isAdministrator: user.isAdministrator
+                                ?'<span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>'
+                                : "",
+                              href: "/#!users/profile?id=" + user.id
+                            },
+                            {
+                              append: true
+                            });
+                      }
+                  }}
+                );
+              })
+            }
+          }
         })
-      }
     },
     error: function (jqXHR, textStatus, errorThrown) {
         showAlert(textStatus + ': ' + errorThrown, 'danger', 0)
